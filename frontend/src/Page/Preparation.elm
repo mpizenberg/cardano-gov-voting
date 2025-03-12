526a857ba3e320b09801a3e4d95e450ec1931ada
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2025-03-12 09:26:54 +0100

    Re-add 'Cardano Foundation' mention in ipfs section

--- frontend/src/Page/Preparation.elm
+++ frontend/src/Page/Preparation.elm
@@ -3296,7 +3296,7 @@ viewPermanentStorageStep ctx rationaleSigStep step =
                                 , HA.class "mr-2"
                                 ]
                                 []
-                            , Html.label [ HA.class "text-base" ] [ text "Pre-configured IPFS" ]
+                            , Html.label [ HA.class "text-base" ] [ text "Pre-configured IPFS (Cardano Foundation)" ]
                             ]
                         , div [ HA.class "flex items-center mb-4" ]
                             [ Html.input
